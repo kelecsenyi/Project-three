78817e1711e280ce234ada0fd64b754355275b59
Balázs <kelecsenyibalazs99@gmail.com>
2022-04-10 11:24:03 +0200

    scroll

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-document.getElementById('js-forward').onclick = function(){
-    document.getElementById("js-films").scrollLeft += 800;
-}
+var divs = document.querySelectorAll('.films');
+var forwards = document.querySelectorAll("#js-forward");
+var backwards = document.querySelectorAll("#js-backward");
+
+Array.prototype.forEach.call(forwards, function (forward, index) {
+    forward.addEventListener('click', function () {
+        Array.prototype.forEach.call(divs, function (div, i) {
+            if (i === index) {
+                divs[i].scrollLeft += 800;
+            }                
+        });
+    })
+});
+
+Array.prototype.forEach.call(backwards, function (backward, index) {
+    backward.addEventListener('click', function () {
+        Array.prototype.forEach.call(divs, function (div, i) {
+            if (i === index) {
+                divs[i].scrollLeft -= 800;
+            }                
+        });
+    })
+});
